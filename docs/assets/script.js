a71c3e3458c14254e1cc68f6024b3bd267bebba7
Mickael Knop <knopmickael@gmail.com>
2023-08-17 08:38:53 -0300

    upd cv download links

--- docs/assets/script.js
+++ docs/assets/script.js
@@ -69,7 +69,8 @@ window.addEventListener("DOMContentLoaded", function () {
             I also have experience in agile culture and DevOps, including container management, horizontal scalability, monitoring, creating integration and deployment pipelines, and other principles of distributed applications.
           </p>
         `;
-        document.querySelector("#about-cv-link").setAttribute('href', './assets/mickael-knop-enus.pdf')
+        document.querySelector("#about-cv-link").href =
+          "./assets/mickael-knop-enus.pdf";
         document.querySelector("#about-cv-btn").innerText =
           "Download my CV here";
 
@@ -80,8 +81,9 @@ window.addEventListener("DOMContentLoaded", function () {
           getJobContent(data);
 
         document.querySelector("#projects-title").innerText = "Side projects";
-        
-        document.querySelector("#footer-text").innerText = "All Rights Reserved.";
+
+        document.querySelector("#footer-text").innerText =
+          "All Rights Reserved.";
         break;
       case "pt-BR":
         document.querySelector("#hero-title").innerText =
@@ -100,7 +102,8 @@ window.addEventListener("DOMContentLoaded", function () {
             Também possuo experiência em cultura ágil e DevOps, incluindo gerenciamento de containers, escalabilidade horizontal, monitoramento, criação de pipelines de integração e de deployment, entre outros princípios de aplicações distribuídas.
           </p>
         `;
-        document.querySelector("#about-cv-link").setAttribute('href', './assets/mickael-knop-ptbr.pdf')
+        document.querySelector("#about-cv-link").href =
+          "./assets/mickael-knop-ptbr.pdf";
         document.querySelector("#about-cv-btn").innerText =
           "Baixe meu currículo";
 
@@ -113,7 +116,8 @@ window.addEventListener("DOMContentLoaded", function () {
         document.querySelector("#projects-title").innerText =
           "Projetos pessoais";
 
-        document.querySelector("#footer-text").innerText = "Todos direitos reservados.";
+        document.querySelector("#footer-text").innerText =
+          "Todos direitos reservados.";
         break;
     }
   };
